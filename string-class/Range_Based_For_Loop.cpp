715bc42979822a61d424f3ca6e69b184fca6e359
majharul <majharuljony163@gmail.com>
2024-11-24 22:07:09 +0600

    learn strin constructor and range based for loop

---
+++ string-class/Range_Based_For_Loop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    string s = "hello";
+    for (char ch : s)
+    {
+        cout << ch << " ";
+    }
+
+    return 0;
+}
---
+++ string-class/String_Constructor.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s("hello", 2);
+    // string s(5, 'a');
+    // string s(5, 'a');
+
+    string s = "world";
+    reverse(s.begin(), s.end());
+    cout << s;
+
+    return 0;
+}
